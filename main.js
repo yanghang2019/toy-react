a7860447d9e71aa4b7bc4da8e5cecc69fada110a
杨航 <yanghang.a@mininglamp.com>
2020-11-11 12:15:46 +0800

    feat of react compoment

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-let 
+import {createElement,render,Component} from "./toy-react"
+
+// for(let i of [1,2,3]) {
+//     console.log(i);
+// }
+
+class MyComponent extends Component{
+
+    render() {
+        return <div>
+            <h1>my component</h1>
+            {
+                this.children
+            }
+        </div>
+    }
+}
+
+
+
+
+// window.a = <div id="a" class="c">
+//     <div>a</div>
+//     <div>b</div>
+//     <div></div>
+
+// </div>
+
+// document.body.appendChild(<MyComponent id="a" class="c">
+//     <div>a</div>
+//     <div>b</div>
+//     <div></div>
+
+// </MyComponent>)
+
+render(<MyComponent id="a" class="c">
+    <div>a</div>
+    <div>b</div>
+    <div></div>
+
+</MyComponent>,document.body)
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ module.exports = {
     entry: {
         main: "./main.js"
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        //preset可以理解为是babel的快捷方式 一个preset包含多的plugin
+                        presets: ["@babel/preset-env"],
+                        plugins: [['@babel/plugin-transform-react-jsx', {pragma: "createElement"}]]
+                    }
+                }
+            }
+        ]
+    },
     mode: "development",
     optimization: {
         minimize:false
